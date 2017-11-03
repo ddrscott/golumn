d0a59dca2d70c42432b0e2cb466ff61ee7a3c3f6
Scott Pierce <scott.pierce@centro.net>
2017-11-03 05:58:17 -0500

    Update classifications

--- setup.py
+++ setup.py
@@ -44,22 +44,25 @@ setup(
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
+        'Environment :: MacOS X :: Cocoa',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: User Interfaces',
         'Topic :: Desktop Environment',
         'Intended Audience :: Developers',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
     ],
 
     # What does your project relate to?
