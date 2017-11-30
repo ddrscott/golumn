d10644ed9a8f1db0775b79bc67007e73142800e3
Scott Pierce <scott.pierce@centro.net>
2017-11-30 04:08:07 -0600

    Align numeric columns to the right (Issue #5)

--- golumn/Utils.py
+++ golumn/Utils.py
@@ -26,5 +26,18 @@ def center_on_active_display(frm, size):
     x2, y2, w2, h2 = active_area
     x1 = int((w2 - w1) / 2)
     y1 = int((h2 - h1) / 2)
-    print('w1:', w1, ', h1:', h1, ', x2:', x2, ', y2:', y2, ', h2:', h2, ', w2:', w2, ', x1:', x1, ', y1:', y1)
+    print('center_on_active_display => w1:', w1, ', h1:', h1, ', x2:', x2, ', y2:', y2, ', h2:', h2, ', w2:', w2, ', x1:', x1, ', y1:', y1)
     frm.SetPosition((x2 + x1, y2 + y1))
+
+
+def unique_array(items):
+    """
+    If an item is duplicated, it appends a numeric suffix to it.
+        f(items=(a, b, a, c)) => (a, b, a1, c)
+    """
+    counts = dict()
+    result = list()
+    for i in items:
+        counts[i] = counts.get(i, -1) + 1
+        result.append(str(i) + str(counts[i]) if counts[i] > 0 else i)
+    return result
