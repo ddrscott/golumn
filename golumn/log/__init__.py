57c82d6ed96d0ca4bc343082802e41c2b49019da
Scott Pierce <scott.pierce@centro.net>
2017-12-13 08:36:05 -0600

    Force context to golumn

--- golumn/log/__init__.py
+++ golumn/log/__init__.py
@@ -3,15 +3,8 @@
 import syslog
 import wx
 
-# set the context to the executable name
-context_name = None
-
 
 def log(txt, lvl=syslog.LOG_NOTICE):
-    global context_name
-    if context_name is None:
-        app = wx.GetApp()
-        context_name = app and app.GetAppName()
-    syslog.openlog(str(context_name))
+    syslog.openlog('golumn')
     syslog.syslog(lvl, txt)
     wx.LogDebug(txt)
